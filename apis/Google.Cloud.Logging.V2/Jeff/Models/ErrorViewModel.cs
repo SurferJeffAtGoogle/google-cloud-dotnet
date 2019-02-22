4d0bb293c3ac712a209e8f85161c9ebf9c75606d
Jeffrey Rennie <rennie@google.com>
2019-02-22 10:53:44 -0800

    An ASP.NET MVC project that depends on the logger.

---
+++ apis/Google.Cloud.Logging.V2/Jeff/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jeff.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
